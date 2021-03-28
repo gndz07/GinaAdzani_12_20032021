2257b756b9aaa525a996790967475db6b13f6b91
Gina A <ginadzani@gmail.com>
2021-03-28 20:24:39 +0200

    Add activityChart component

---
+++ src/components/ActivityChart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+
+export default class ActivityChart extends React.Component {
+
+	render() {
+		const data = this.props.chart;
+		const newData = data.map(datum => ({number: data.indexOf(datum)+1 ,...datum}));
+
+		return (
+			<div id="chart-parent">
+			<h2 className="chart-title">Activité quotidienne</h2>
+			<ResponsiveContainer width={'100%'} height={320}>
+				<BarChart width={835} height={320} data={newData}
+	      		margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+	      			<CartesianGrid strokeDasharray="3 3" />
+	      			<XAxis dataKey="number" />
+	      			<YAxis orientation="right" />
+	      			<Tooltip />
+	      			<Legend verticalAlign="top" align="right" iconType="circle" iconSize="10"/>
+	      			<Bar name="Poids (kg)" dataKey="kilogram" fill="#282D30" radius={[10, 10, 0, 0]} barSize={10} barGap={5} />
+	      			<Bar name="Calories brûlées (kCal)" dataKey="calories" fill="#E60000" radius={[10, 10, 0, 0]} barSize={10} barGap={5} />
+	    		</BarChart>
+	    		</ResponsiveContainer>
+	    	</div>
+		)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header.js'
 import SideNav from './components/SideNav.js'
 import UserPage from './pages/UserPage.js'
