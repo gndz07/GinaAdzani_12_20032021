60d10b7c7fd4adeac419e8aecadea0cec75b7607
Gina A <ginadzani@gmail.com>
2021-04-01 15:45:51 +0200

    Add API fetch function

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -22,35 +22,75 @@ export default class UserPage extends React.Component {
 	constructor(props) {
     	super(props);
    		this.state = {
-   			data: userData.find((user) => user.id == this.props.match.params.userId),
-   			activity: userActivity.find((user) => user.userId == this.props.match.params.userId),
-   			average: userAverage.find((user) => user.userId == this.props.match.params.userId),
-   			performance: userPerformance.find((user) => user.userId == this.props.match.params.userId),
+   			error: null,
+   			isLoaded: false,
+   			data: [],
+   			activity: [],
+   			average: [],
+   			performance: []
    		};
   	}
 
+  	componentDidMount() {
+  		const userId = this.props.match.params.userId;
+  		Promise.all([
+  			fetch(`http://localhost:3000/user/${userId}`),
+  			fetch(`http://localhost:3000/user/${userId}/activity`),
+  			fetch(`http://localhost:3000/user/${userId}/average-sessions`),
+  			fetch(`http://localhost:3000/user/${userId}/performance`)
+  		])
+  		.then(([result1, result2, result3, result4]) => Promise.all(
+  			[result1.json(), result2.json(), result3.json(), result4.json()]))
+  		.then(
+  			([data1, data2, data3, data4]) => {
+	  			this.setState({
+		  			isLoaded: true,
+		  			data: data1,
+		  			activity: data2,
+		  			average: data3,
+		  			performance: data4
+	  			})
+	  		},
+	  		(error) => {
+	  			this.setState({
+	  				isLoaded: true,
+	  				error
+	  			})
+	  		}
+  		)
+  	}
+
 	render() {
-		return (
-			<div id="main">
-				<UserName user={this.state.data.userInfos} />
-				<div id="statistics-parent">
-					<div id="graphics">
-						<ActivityChart data={this.state.activity.sessions} />
-						<div id="three-graphs-container">
-							<AverageStat data={this.state.average.sessions} />
-							<Performance data={this.state.performance.data} />
-							<Percentage data={this.state.data} />
+		console.log(this.state);
+		const {error, isLoaded} = this.state;
+		if (error) {
+			return <div>Error loading page</div>;
+		}
+		if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div id="main">
+					<UserName user={this.state.data.data.userInfos} />
+					<div id="statistics-parent">
+						<div id="graphics">
+							<ActivityChart data={this.state.activity.data.sessions} />
+							<div id="three-graphs-container">
+								<AverageStat data={this.state.average.data.sessions} />
+								<Performance data={this.state.performance.data.data} />
+								<Percentage data={this.state.data.data} />
+							</div>
+						</div>
+
+						<div id="stat-container">
+							<PersonalStat name="Calories" icon={caloriesIcon} data={this.state.data.data.keyData.calorieCount} unit="kCal"/>
+							<PersonalStat name="Proteines" icon={proteinIcon} data={this.state.data.data.keyData.proteinCount} unit="g"/>
+							<PersonalStat name="Glucides" icon={carbsIcon} data={this.state.data.data.keyData.carbohydrateCount} unit="g"/>
+							<PersonalStat name="Lipides" icon={fatIcon} data={this.state.data.data.keyData.lipidCount} unit="g"/>
 						</div>
 					</div>
-
-					<div id="stat-container">
-						<PersonalStat name="Calories" icon={caloriesIcon} data={this.state.data.keyData.calorieCount} unit="kCal"/>
-						<PersonalStat name="Proteines" icon={proteinIcon} data={this.state.data.keyData.proteinCount} unit="g"/>
-						<PersonalStat name="Glucides" icon={carbsIcon} data={this.state.data.keyData.carbohydrateCount} unit="g"/>
-						<PersonalStat name="Lipides" icon={fatIcon} data={this.state.data.keyData.lipidCount} unit="g"/>
-					</div>
 				</div>
-			</div>
-		)
+			)
+		}	
 	}
 }
